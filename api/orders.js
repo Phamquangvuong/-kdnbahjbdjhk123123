3026fc953bf1703d30825763c58adee1083ad910
Phamquangvuong <phamquangvuongdev@gmail.com>
2026-04-26 12:55:57 +0700

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,14 +1,12 @@
 const redis = require("../lib/db");
 
 module.exports = async (req, res) => {
-
   const keys = await redis.keys("pay:*");
-  const data = [];
 
+  const data = [];
   for (const k of keys) {
-    const item = await redis.get(k);
-    data.push(item);
+    data.push(await redis.get(k));
   }
 
-  res.json(data.sort((a,b)=>b.createdAt-a.createdAt));
+  res.json(data);
 };
