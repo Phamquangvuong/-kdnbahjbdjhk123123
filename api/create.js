954cf4554efea33d1d8b6e3500340916d5076afe
Phamquangvuong <phamquangvuongdev@gmail.com>
2026-04-26 13:19:10 +0700

    Update create.js

--- api/create.js
+++ api/create.js
@@ -1,21 +1,24 @@
-const redis = require("../lib/db");
+const EXPIRE_TIME = 2 * 60 * 1000; // 2 phút
 
 module.exports = async (req, res) => {
   const amount = Number(req.query.amount);
-
   if (!amount) return res.json({ error: "invalid amount" });
 
   const note = "nap" + Date.now();
 
-  await redis.set(`pay:${note}`, {
+  const data = {
     note,
     amount,
     status: "pending",
-    createdAt: Date.now()
-  }, { ex: 600 });
+    createdAt: Date.now(),
+    expireAt: Date.now() + EXPIRE_TIME
+  };
+
+  await redis.set(`pay:${note}`, data, { ex: 180 }); // TTL Redis
 
   res.json({
     qr: `https://img.vietqr.io/image/MB-0975868667-compact2.png?amount=${amount}&addInfo=${note}`,
-    note
+    note,
+    expireAt: data.expireAt
   });
 };
