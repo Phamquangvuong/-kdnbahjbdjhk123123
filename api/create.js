4fcd3f4771db910b501f972f6710d8068a5457d4
Phamquangvuong <phamquangvuongdev@gmail.com>
2026-04-26 12:55:40 +0700

    Update create.js

--- api/create.js
+++ api/create.js
@@ -1,27 +1,21 @@
 const redis = require("../lib/db");
 
 module.exports = async (req, res) => {
-
   const amount = Number(req.query.amount);
 
-  if (!amount || amount < 1000) {
-    return res.json({ error: "invalid amount" });
-  }
+  if (!amount) return res.json({ error: "invalid amount" });
 
   const note = "nap" + Date.now();
 
-  const data = {
+  await redis.set(`pay:${note}`, {
     note,
     amount,
     status: "pending",
     createdAt: Date.now()
-  };
-
-  await redis.set(`pay:${note}`, data, { ex: 600 });
+  }, { ex: 600 });
 
   res.json({
     qr: `https://img.vietqr.io/image/MB-0975868667-compact2.png?amount=${amount}&addInfo=${note}`,
-    note,
-    amount
+    note
   });
 };
