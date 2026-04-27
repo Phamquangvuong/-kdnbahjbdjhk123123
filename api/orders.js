a66426b3f9401344c0a7f68a510d8ac4ee8aebe5
Phamquangvuong <phamquangvuongdev@gmail.com>
2026-04-27 12:22:55 +0700

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,12 +1,23 @@
-const redis = require("../lib/db");
+const redis = require("../lib/redis");
 
 module.exports = async (req, res) => {
-  const keys = await redis.keys("pay:*");
+  try {
+    const keys = await redis.keys("pay:*");
+    const data = await Promise.all(keys.map(k => redis.get(k)));
 
-  const data = [];
-  for (const k of keys) {
-    data.push(await redis.get(k));
+    const totalOrders = data.length;
+    const paidOrders = data.filter(i => i.status === "paid").length;
+    const income = data.reduce((sum, i) =>
+      i.status === "paid" ? sum + i.amount : sum, 0
+    );
+
+    res.json({
+      totalOrders,
+      paidOrders,
+      income
+    });
+
+  } catch (e) {
+    res.json({ error: e.message });
   }
-
-  res.json(data);
 };
