08e95a0c89ad2894b1f503ded8777294c01e708a
Phamquangvuong <phamquangvuongdev@gmail.com>
2026-04-27 13:01:13 +0700

    Update create.js

--- api/create.js
+++ api/create.js
@@ -3,16 +3,35 @@ const redis = require("../lib/redis");
 module.exports = async (req, res) => {
   const amount = Number(req.query.nap);
 
-  if (!amount) return res.json({ error: "invalid amount" });
+  if (!amount || amount <= 0) {
+    return res.json({ error: "invalid amount" });
+  }
+
+  const ip =
+    req.headers["x-forwarded-for"] ||
+    req.connection?.remoteAddress ||
+    "ip";
+
+  const key = "limit:" + ip;
+
+  const count = await redis.get(key) || 0;
+
+  if (count > 5) {
+    return res.json({ error: "too many requests" });
+  }
+
+  await redis.set(key, count + 1, { ex: 10 });
 
   const note = "nap" + Date.now();
 
-  await redis.set("pay:" + note, {
+  const data = {
     note,
     amount,
     status: "pending",
     expire: Date.now() + 120000
-  });
+  };
+
+  await redis.set("pay:" + note, data, { ex: 300 });
 
   res.json({
     qr: `https://img.vietqr.io/image/MB-0975868667-compact2.png?amount=${amount}&addInfo=${note}`,
